9b8a035fa618cc2508e37243894e57af7e9e491d
LinZH <lzh_xmu@163.com>
2024-07-25 09:48:25 +0800

    Initial Commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MusicStore.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        // Initialize the ViewModel
+    }
+
+    [RelayCommand]
+    private void BuyMusic()
+    {
+        Console.WriteLine("Music bought!");
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MusicStore.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
